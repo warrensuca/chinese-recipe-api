404962fe56eb2c903c14ef399292ec6804f8d161
warrensuca <155809812+warrensuca@users.noreply.github.com>
2026-06-20 18:34:21 -0700

    add rate limiting

--- api/main.py
+++ api/main.py
@@ -1,13 +1,49 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Response
 import pandas as pd
 import numpy as np
 import ast
 import joblib 
-from typing import Optional
+from typing import Optional, Dict
 import os
+from starlette.middleware.base import BaseHTTPMiddleware
+import time
+from collections import defaultdict
 
 app = FastAPI(title="Recipe Recommendation API")
 
+#middleware
+class CustomMiddleWare(BaseHTTPMiddleware):
+    def __init__(self, app):
+        super().__init__(app)
+        self.rate_limit_records: Dict[str, float] = defaultdict(float)
+    
+    async def log_message(self, message: str):
+        print(message)
+
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.client.host
+        current_time = time.time()
+
+        if current_time - self.rate_limit_records[client_ip] < 1: 
+            return Response(content = "Rate limit exceeded", status_code = 429)
+
+        self.rate_limit_records[client_ip] = current_time
+        path = request.url.path
+        await self.log_message(f"Request to {path}")
+
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+
+        custom_headers = {"X-Process-Time": str(process_time)}
+        for header, value in custom_headers.items():
+            response.headers.append(header, value)
+
+        await self.log_message(f"Response for {path} took {process_time} seconds")
+
+        return response
+
+app.add_middleware(CustomMiddleWare)
 #load data and models
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
